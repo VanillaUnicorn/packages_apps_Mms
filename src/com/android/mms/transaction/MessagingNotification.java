omnirom/android_packages_apps_Mms

[2/4] Mms: Breathing missedcall/sms/voicemail
Change-Id: Id12182719315a6e5bcbabd8f843eff70c8c3ed3a

src/com/android/mms/transaction/MessagingNotification.java
@@ -52,6 +52,7 @@ import android.os.Handler;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.provider.Telephony.Mms;
 import android.provider.Telephony.Sms;
 import android.telephony.TelephonyManager;
@@ -959,7 +960,12 @@ public class MessagingNotification {
             taskStackBuilder.addNextIntent(mostRecentNotification.mClickIntent);
         }
         // Always have to set the small icon or the notification is ignored
-        noti.setSmallIcon(R.drawable.stat_notify_sms);
+        if (Settings.System.getInt(context.getContentResolver(),
+            Settings.System.KEY_SMS_BREATH, 0) == 1) {
+            noti.setSmallIcon(R.drawable.stat_notify_sms_breath);
+          } else {
+            noti.setSmallIcon(R.drawable.stat_notify_sms);
+        }
 
         NotificationManager nm = (NotificationManager)
                 context.getSystemService(Context.NOTIFICATION_SERVICE);

VanillaUnicorn/packages_apps_Mms

src/com/android/mms/transaction/MessagingNotification.java
@@ -52,6 +52,7 @@ import android.os.Handler;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.provider.Telephony.Mms;
 import android.provider.Telephony.Sms;
 import android.telephony.TelephonyManager;
@@ -961,7 +962,12 @@ public class MessagingNotification {
                     PendingIntent.FLAG_UPDATE_CURRENT);
         }
         // Always have to set the small icon or the notification is ignored
-        noti.setSmallIcon(R.drawable.stat_notify_sms);
+        if (Settings.AOKP.getInt(context.getContentResolver(),
+            Settings.AOKP.KEY_SMS_BREATH, 0) == 1) {
+            noti.setSmallIcon(R.drawable.stat_notify_sms_breath);
+          } else {
+            noti.setSmallIcon(R.drawable.stat_notify_sms);
+        }
 
         NotificationManager nm = (NotificationManager)
                 context.getSystemService(Context.NOTIFICATION_SERVICE);